39a92c40260ca4cc09a1981cfeba1503064b94d8
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2015-10-17 15:08:48 +0800

    Heap Sort
    Signed-off-by: Kenneth Ham <kenneth.ham@comp.nus.edu.sg>

--- app.rb
+++ app.rb
@@ -1,15 +1,20 @@
 require_relative 'lib/dsag'
 
 class App
+  # Result
+  result = []
+
   # Insertion Sort
-  numbers = [5, 2, 8, 4, 6, 1, 10, 3, 9, 7, 11, 12]
+  random_numbers = [5, 2, 8, 4, 6, 1, 10, 3, 9, 7, 11, 12]
+  numbers = [27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0]
 
   insertion_sort = InsertionSort.new
-  insertion_sort.sort(numbers)
+  insertion_sort.sort(random_numbers.clone)
 
   merge_sort = MergeSort.new
-  merge_sort.sort(numbers)
+  merge_sort.sort(random_numbers.clone)
 
   heap = Heap.new
-  puts heap.max_heapify(numbers)
+  heap.sort(numbers.clone, result)
+  p result
 end
--- lib/heap.rb
+++ lib/heap.rb
@@ -1,26 +1,60 @@
 class Heap
-  def max_heapify(numbers)
-    i = numbers.length
-    left = 2 * i
-    right = 2 * i + 1
-    largest = i
+  public
 
-    if left <= numbers.length && numbers[left] > numbers[largest]
-      largest = left
+  def sort(numbers, result)
+    i = numbers.length / 2 - 1
+
+    while i >= 0
+      heapify(numbers, i)
+      i -= 1
     end
 
-    if right <= numbers.length && numbers[right] > numbers[largest]
-      largest = right
+    result.push(adjust_heap(numbers))
+    sort(numbers, result) if numbers.length > 0
+  end
+
+  private
+
+  def heapify(numbers, i)
+    numbers.each do
+      i = (i / 2).floor
+      left_index = (2 * i + 1)
+      right_index = (2 * i + 2)
+
+      if left_index <= numbers.length - 1 && numbers[left_index] > numbers[i]
+        largest = left_index
+      else
+        largest = i
+      end
+
+      if right_index <= numbers.length - 1 && numbers[right_index] > numbers[largest]
+        largest = right_index
+      end
+
+      if numbers[largest] != numbers[i]
+        numbers[i], numbers[largest] = numbers[largest], numbers[i]
+        heapify(numbers, largest)
+      end
     end
+  end
 
-    if largest != i
-      temp = numbers[i]
-      numbers[i] = numbers[largest]
-      numbers[largest] = temp
+  private
 
-      max_heapify(numbers)
+  def build_heap(numbers)
+    numbers.size.downto(0) do |index|
+      heapify(numbers, index)
     end
 
     numbers
+  end
+
+  private
+
+  def adjust_heap(numbers)
+    top = numbers[0]
+    numbers[0], numbers[-1] = numbers[-1], numbers[0]
+    numbers.pop
+
+    top
   end
 end
